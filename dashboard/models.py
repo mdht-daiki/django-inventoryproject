07e5c433982e6e43ca2ce978c696edf14ab430e4
dyamagiwa <Daiki.Yamagiwa@skyworksinc.com>
2025-09-26 17:03:54 +0900

    edited admin screen

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
