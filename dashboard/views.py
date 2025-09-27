215a1369a481c5672a0eb654b79cb3254b1dce13
dkmdhtWin <dkyamagiwa156@gmail.com>
2025-09-27 16:29:23 +0900

    added detail views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Order
 from .forms import ProductForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,7 +13,19 @@ def index(request):
 
 @login_required
 def staff(request):
-    return render(request, 'dashboard/staff.html')
+    workers = User.objects.all()
+    context = {
+        'workers': workers
+    }
+    return render(request, 'dashboard/staff.html', context)
+
+@login_required
+def staff_detail(request, pk):
+    worker = User.objects.get(id=pk)
+    context = {
+        'worker': worker,
+    }
+    return render(request, 'dashboard/staff_detail.html', context)
 
 @login_required
 def product(request):
@@ -33,6 +46,7 @@ def product(request):
     }
     return render(request, 'dashboard/product.html', context)
 
+@login_required
 def product_delete(request, pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -40,6 +54,7 @@ def product_delete(request, pk):
         return redirect('dashboard-product')
     return render(request, 'dashboard/product_delete.html')
 
+@login_required
 def product_update(request, pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -56,4 +71,8 @@ def product_update(request, pk):
 
 @login_required
 def order(request):
-    return render(request, 'dashboard/order.html')
+    orders = Order.objects.all()
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'dashboard/order.html', context)
